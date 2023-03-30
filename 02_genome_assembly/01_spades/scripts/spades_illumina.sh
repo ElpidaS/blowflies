73fb88c6501df3c2fd58e65d199347e502a31a9c
ElpidaS <elpidaskarlou@gmail.com>
2023-03-30 13:37:47 +0000

    spades without correction before - TF one assembly

--- 02_genome_assembly/01_spades/scripts/spades_illumina.sh
+++ 02_genome_assembly/01_spades/scripts/spades_illumina.sh
@@ -9,10 +9,10 @@
 # Date: 23.03.2023
 # Author: Elpida Skarlou
 
-# The input of the script is trimmed Illumina paired-end reads in the TRIMMED directory. The script uses BayesHammer for error correction, and then it run
-# SPAdes with the corrected reads and MismatchCorrector to perform genome assembly for both types of female blowflies. 
-# The output of the script is three assembled genomes for TF  (Male producing Females) and AF (Female producing Females) individuals.
-# The script also transfers the log file and output files to the SPADES directory and removes intermediate files from the scratch directory.
+# This script performs genome assembly for Illumina sequencing data using SPAdes software. It first transfers trimmed Illumina reads to a temporary
+# directory, then performs two separate assemblies for two different sequencing libraries (TF11 and TF19), and one for AF library, respectively. The
+# output assemblies are synced to a final destination and temporary files are deleted. The input files are trimmed Illumina fastq files, and the output
+# files are genome assembly files.
 
 set -e # exit immediately on error
 
@@ -34,35 +34,21 @@ cd $SCRATCH
 rsync -av $TRIMMED/*.trimmed.fastq.gz .
 
 
-#### run BayesHammer for error correction ####
+### run SPAdes with and MismatchCorrector ###
 
 # spades vesrion 3.15.5
+# we did not correct with BayesHammer because short reads are already pretty accurate 
 
-for file in $(ls *1_001.trimmed.fastq.gz)
-do
-	base=$(basename $file "1_001.trimmed.fastq.gz") 
-  	spades.py -1 ${base}1_001.trimmed.fastq.gz -2 ${base}2_001.trimmed.fastq.gz --only-error-correction -o . 
-done
-
-### run SPAdes with BayesHammer-corrected reads and MismatchCorrector ###
-
-# one assembly per library provided
-# 2 for the female producing females (TF)
-# 1 for the male producing females (AF)
-
-
-#### for TF
+#### for TF - One assembly
 TF11="TF11_Chrysomya-rufifacies_S3_"
 TF19="TF19_Chrysomya-rufifacies_S4_"
 
-#TF11
-spades.py -1 ./corrected/${TF11}R1_001.trimmed.corrected.fastq.gz -2 ./corrected/${TF11}R2_001.trimmed.corrected.fastq.gz -o . --threads 16 --isolate --mismatch-correction
-
-#TF19
-spades.py -1 ./corrected/${TF19}R1_001.trimmed.corrected.fastq.gz -2 ./corrected/${TF19}R2_001.trimmed.corrected.fastq.gz -o . --threads 16 --isolate --mismatch-correction
+#  create a single assembly using the reads from both libraries (TF11 and TF19)
+spades.py --pe1-1 ./${TF19}R1_001.trimmed.fastq.gz --pe1-2 ./${TF19}R2_001.trimmed.fastq.gz --pe2-1 ./${TF11}R1_001.trimmed.fastq.gz --pe2-2 ./${TF11}R2_001.trimmed.fastq.gz -o . --threads 16 --mismatch-correction
 
 #### for AF
 AF="AF7_Chrysomya-rufifacies_S2_"
+
 spades.py -1 ./corrected/${AF}R1_001.trimmed.corrected.fastq.gz -2 ./corrected/${AF}R2_001.trimmed.corrected.fastq.gz -o . --threads 16 --isolate --mismatch-correction
 
 # remove all the trimmed reads
